b93c6046fedb3f1d0a10a06957686f8558839401
Gabriel <gabrielrei2112@gmail.com>
2025-09-24 13:36:11 +0100

    Ammo Container + new crosshair

--- scenes_and_scripts/basic_hud.gd
+++ scenes_and_scripts/basic_hud.gd
@@ -1,5 +1,18 @@
 extends Node
-const CROSSHAIR_SQUARE = preload("uid://v5aa0wwmt7xq")
+const CROSSHAIR = preload("uid://dmh6meew0pmd5")
+const AMMO_COUNTER = preload("uid://beuu87rbc6a1r")
+var _ammo_counter_text
+var _health
 
 func _ready() -> void:
-	Input.set_custom_mouse_cursor(CROSSHAIR_SQUARE)
+	Input.set_custom_mouse_cursor(CROSSHAIR)
+	_ammo_counter_text = $Control/Control/Ammo_Counter/Ammo
+	_health = %Health
+	
+	
+	
+#func _process(_delta: float) -> void:
+	#_ammo_counter_text.text = player.getAmmo()
+	#_health.text = player.getHealth()
+	
+	
