43c1a4c453bfdec84a341699a1035ae515bc97cc
DuarteFon <duarte.mfonseca@gmail.com>
2025-09-25 18:02:03 +0100

    im tired boss

--- scenes_and_scripts/gun_scenes/gun.gd
+++ scenes_and_scripts/gun_scenes/gun.gd
@@ -22,24 +22,7 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("fire_gun"):
 		print("pressionado")
 		_on_shoot()
-func _physics_process(delta: float) -> void:
-	if(true): return
-	var mouse_position = get_viewport().get_mouse_position()
-	var ray_origin = camera.project_ray_origin(mouse_position)
-	var ray_target = ray_origin + camera.project_ray_normal(mouse_position) * 2000
-	var params = PhysicsRayQueryParameters3D.new()
-	params.from = ray_origin
-	params.to = ray_target
-	
-	var space_state = get_world_3d().direct_space_state
-	var intersection = space_state.intersect_ray(params)
-	 
-	if intersection:
-		print("NOT EMPTY")
-		var world_position = intersection.position
-		var look_at_me = Vector3(world_position.x, position.y, world_position.z )
-		look_at(look_at_me, Vector3.UP)
-	
+ 
 
 
 func _on_shoot() -> void:
@@ -47,11 +30,11 @@ func _on_shoot() -> void:
 	if shoot_type == GunType.PROJECTILE:
 		print("firing my weapon!!")
 		var bullet_instance:RigidBody3D = BULLET.instantiate()
-		var format_string = "BulletPos %s, PlayerPos: %s, RaycastPos: %s"
 		bullet_instance.position = ray_cast_3d.global_position
-		var node_root := $"../../../../../../.."
-		node_root.add_child(bullet_instance)
  
+		var node_root := get_tree().get_root().get_children()[0]
+		node_root.add_child(bullet_instance)
+ 		
 		bullet_instance.apply_impulse(global_transform.basis.z * BULLET_SPEED)
 		 
 		
--- scenes_and_scripts/player_movement.gd
+++ scenes_and_scripts/player_movement.gd
@@ -56,15 +56,15 @@ func _physics_process(delta):
 func _look_at_crosshair() -> void:
 	var mouse_position = get_viewport().get_mouse_position()
 	var ray_origin = camera.project_ray_origin(mouse_position)
-	var ray_target = ray_origin + camera.project_ray_normal(mouse_position) * 2000
+	var ray_target = ray_origin + camera.project_ray_normal(mouse_position) * 100
 	var params = PhysicsRayQueryParameters3D.new()
+	
 	params.from = ray_origin
 	params.to = ray_target
 	
 	var space_state = get_world_3d().direct_space_state
 	var intersection = space_state.intersect_ray(params)
-	  
-
+	 
 	if intersection:
 		print("NOT EMPTY")
 		var world_position = intersection.position
