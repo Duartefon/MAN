116b5ab4e4823392dedcbb13fbd10f81bd8e2e0d
DuarteFon <duarte.mfonseca@gmail.com>
2025-09-25 14:15:23 +0100

    trying to fix aiming

--- scenes_and_scripts/gun_scenes/gun.gd
+++ scenes_and_scripts/gun_scenes/gun.gd
@@ -1,6 +1,9 @@
 extends Node3D
 @export var shoot_type:GunType
 @onready var ray_cast_3d: RayCast3D = $glock/RayCast3D
+@onready var camera: Camera3D = $"../../../../../../../Camera3D"
+@onready var gun_barrel: Node3D = $glock/GunBarrel
+
 enum GunType {PROJECTILE, HITSCAN}
 
 var bullet_damage:float
@@ -9,15 +12,34 @@ var total_ammo:float
 const BULLET_SPEED:float = 14
 const BULLET = preload("res://scenes_and_scripts/gun_scenes/bullet.tscn")
  
-@onready var gun_barrel: Node3D = $glock/GunBarrel
 
+ 
 
+ 
 func _on_reload() -> void:
 	pass
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("fire_gun"):
 		print("pressionado")
 		_on_shoot()
+func _physics_process(delta: float) -> void:
+	if(true): return
+	var mouse_position = get_viewport().get_mouse_position()
+	var ray_origin = camera.project_ray_origin(mouse_position)
+	var ray_target = ray_origin + camera.project_ray_normal(mouse_position) * 2000
+	var params = PhysicsRayQueryParameters3D.new()
+	params.from = ray_origin
+	params.to = ray_target
+	
+	var space_state = get_world_3d().direct_space_state
+	var intersection = space_state.intersect_ray(params)
+	 
+	if intersection:
+		print("NOT EMPTY")
+		var world_position = intersection.position
+		var look_at_me = Vector3(world_position.x, position.y, world_position.z )
+		look_at(look_at_me, Vector3.UP)
+	
 
 
 func _on_shoot() -> void:
--- scenes_and_scripts/player_movement.gd
+++ scenes_and_scripts/player_movement.gd
@@ -12,7 +12,9 @@ extends CharacterBody3D
 var target_velocity = Vector3.ZERO
 var horizontal_velocity = Vector3.ZERO
 
- 
+@onready var man: Node3D = $man
+
+@onready var ver_direcaodo_player: CSGBox3D = $verDirecaodoPlayer
 
 
 
@@ -47,27 +49,26 @@ func _physics_process(delta):
 
 	# Moving the Character
 	velocity = horizontal_velocity
+	_look_at_crosshair()
+	
 	move_and_slide()
  
-func _process(delta: float) -> void:
-	# Get mouse position and create a ray from the camera
-	var mouse_pos = get_viewport().get_mouse_position()
-	var ray_origin = camera.project_ray_origin(mouse_pos)
-	var ray_direction = camera.project_ray_normal(mouse_pos) * 500
-	var ray_query = PhysicsRayQueryParameters3D.create(ray_origin, ray_origin + ray_direction)
+func _look_at_crosshair() -> void:
+	var mouse_position = get_viewport().get_mouse_position()
+	var ray_origin = camera.project_ray_origin(mouse_position)
+	var ray_target = ray_origin + camera.project_ray_normal(mouse_position) * 2000
+	var params = PhysicsRayQueryParameters3D.new()
+	params.from = ray_origin
+	params.to = ray_target
 	
 	var space_state = get_world_3d().direct_space_state
-	var ray_result = space_state.intersect_ray(ray_query)
-	
-	if !ray_result.is_empty():
-		var target_position = ray_result.position
-		
-		# Only calculate rotation on the horizontal (X-Z) plane
-		var direction = (target_position - global_transform.origin).normalized()
-		var angle = atan2(direction.x, direction.z)
-		
-		# Apply the rotation to the player's Y-axis
-		rotation.y = angle 
+	var intersection = space_state.intersect_ray(params)
+	  
 
+	if intersection:
+		print("NOT EMPTY")
+		var world_position = intersection.position
+		var look_at_me = Vector3(world_position.x, position.y, world_position.z )
+		look_at(look_at_me, Vector3.UP)
 		 
 		
