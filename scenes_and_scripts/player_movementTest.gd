cda6b7d0c5ef745383666584903f7248cc5c6857
Gabriel <gabrielrei2112@gmail.com>
2025-09-27 13:36:23 +0100

    better movement

--- scenes_and_scripts/player_movementTest.gd
+++ scenes_and_scripts/player_movementTest.gd
@@ -4,11 +4,9 @@ extends CharacterBody3D
 
 @onready var camera: Camera3D = $"../Camera3D"
 
-
 @export var speed = 25
-@export var h_accelerate = 25
-@export var h_desaccelerate = 25
-@export var fall_acceleration = 75
+@export var h_accelerate = 50
+@export var h_desaccelerate = 50
 
 var target_velocity = Vector3.ZERO
 var horizontal_velocity = Vector3.ZERO
@@ -30,40 +28,25 @@ func _ready() -> void:
 	print(gun_script)
 	
 func _physics_process(delta):
-	# Obtain player inputs
 	var direction = Vector3.ZERO
-	#print(direction, speed)
-	direction.x = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
-	direction.z = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
-	
-	#Normalize so that diagonal speed isnt greater than straight speed
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 
-	# Obtain Targeted horizontal velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
-	
-	if target_velocity.length()>0: # Accelerate smoothly towards the Targeted horizontal velocity
-		horizontal_velocity.x = move_toward(horizontal_velocity.x, target_velocity.x, h_accelerate*delta)
-		horizontal_velocity.z = move_toward(horizontal_velocity.z, target_velocity.z, h_accelerate*delta)
-	else: 	# Desaccelerate smoothly when there is none Targeted horizontal velocity
-		horizontal_velocity.x = move_toward(horizontal_velocity.x, target_velocity.x, h_desaccelerate*delta)
-		horizontal_velocity.z = move_toward(horizontal_velocity.z, target_velocity.z, h_desaccelerate*delta)
-		
-	# Vertical Velocity
-	if not is_on_floor(): # If in the air, fall towards the floor.
-		velocity.y -= fall_acceleration * delta
-	else:
-		velocity.y = 0
 
-	# Moving the Character
-	velocity = horizontal_velocity
+	target_velocity = direction * speed
+
+	var accel = h_accelerate if direction != Vector3.ZERO else h_desaccelerate
+
+	velocity.x = move_toward(velocity.x, target_velocity.x, accel * delta)
+	velocity.z = move_toward(velocity.z, target_velocity.z, accel * delta)
+
 	_look_at_crosshair()
 	move_and_slide()
 
-	#Animations, blend tree
-	var move_input_2d = Vector2(
+	var _move_input_2d = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	)
@@ -86,14 +69,10 @@ func handle_anim(delta):
 		RUN:
 			run_val = lerpf(run_val, 1, blend_speed*delta)
 			update_animations(run_val)
-	
-	
 
-func update_animations(run_val):
-	anim_tree["parameters/IdleRunBlend/blend_amount"] = run_val
+func update_animations(run_value):
+	anim_tree["parameters/IdleRunBlend/blend_amount"] = run_value
 
-	
-	
 func _look_at_crosshair():
 	var mouse_pos = get_viewport().get_mouse_position()
 	var ray_origin = camera.project_ray_origin(mouse_pos)
