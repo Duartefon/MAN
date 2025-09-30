47d951d3f75690bc621ef380566986e328a43086
DuarteFon <duarte.mfonseca@gmail.com>
2025-09-30 10:20:58 +0100

    can now skip intro

--- scenes_and_scripts/intro.gd
+++ scenes_and_scripts/intro.gd
@@ -3,3 +3,8 @@ extends Node2D
 
 func _on_video_stream_player_finished() -> void:
 	get_tree().change_scene_to_file("res://scenes_and_scripts/level1.tscn")
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://scenes_and_scripts/level1.tscn")
+		
